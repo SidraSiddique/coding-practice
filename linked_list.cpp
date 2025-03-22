c66e48dc70f5c7f927f17bdf56b9077ae2d642af
sidra siddique <sidrasiddique494@gmail.com>
2025-03-22 19:04:19 +0500

    file is changed

--- linked_list.cpp
+++ linked_list.cpp
@@ -2,62 +2,58 @@
 
 using namespace std;
 
-class Node{
+class Node {
 public:
-	int val;
-	Node *next; 
+    int val;
+    Node *next;
 };
 
-int main(){
-	Node first, second, third, fourth, fifth, sixth;
-	first.val = 1;
-	second.val = 2;
-	third.val = 3;
-	fourth.val = 4;
-	fifth.val = 5;
-	sixth.val = 6;
-	
-	first.next = &second;
-	second.next = &third;
-	third.next = &fourth;
-	fourth.next = &fifth;
-	fifth.next = &sixth;
-	sixth.next = NULL;
-	//change node
-	for (Node *temp = &first ; temp != NULL ; temp = temp -> next)
-		cout << temp -> val << ' ';
-	cout << "\n";
-    first.next=&fourth;
-	fourth.next=&third;
-	three.next=&sixth;
-	six.next=&second
-	for (Node *temp=&first;temmp !=Null; temp -> next)
-	    cout << temp -> val << ' ';
-	cout << "\n";
-    // remove node
-	fourth.next=&sixth;
-	for (Node *temp=&first;temmp !=Null; temp -> next)
-	    cout << temp -> val << ' ';
-	cout << "\n";
+int main() {
+    Node first, second, third, fourth, fifth, sixth;
 
-	// insert another node
-	Node second_third;
-	val.second_third=50;
-	seconed.next=&second_third;
-	second_third.next=&third;
-	for (Node *temp=&first;temmp !=Null; temp -> next)
-	    cout << temp -> val << ' ';
-	cout << "\n";
+    first.val = 1;
+    second.val = 2;
+    third.val = 3;
+    fourth.val = 4;
+    fifth.val = 5;
+    sixth.val = 6;
 
+    first.next = &second;
+    second.next = &third;
+    third.next = &fourth;
+    fourth.next = &fifth;
+    fifth.next = &sixth;
+    sixth.next = NULL;
+    for (Node *temp = &first; temp != NULL; temp = temp->next)
+        cout << temp->val << ' ';
+    cout << "\n";
 
-	return 0;
+    // Change node 
+    first.next = &fourth;
+    fourth.next = &third;
+    third.next = &sixth;
+    sixth.next = &second;
 
+    for (Node *temp = &first; temp != NULL; temp = temp->next)
+        cout << temp->val << ' ';
+    cout << "\n";
 
+    // Remove node 
+    fourth.next = &sixth;
 
+    for (Node *temp = &first; temp != NULL; temp = temp->next)
+        cout << temp->val << ' ';
+    cout << "\n";
 
+    // Insert another node
+    Node second_third;
+    second_third.val = 50;
+    second.next = &second_third;
+    second_third.next = &third;
 
+    for (Node *temp = &first; temp != NULL; temp = temp->next)
+        cout << temp->val << ' ';
+    cout << "\n";
 
-
-
-
+    return 0;
 }
