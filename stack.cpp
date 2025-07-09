cfe8081069a5cb29b34becbcf564099698315435
Sidra Siddique <sidrasiddique494@gmail.com>
2025-07-09 19:58:15 +0500

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -13,3 +13,20 @@ public:
     Stack() {
         top = -1;
     }
+void push(int value) {
+        if (top >= SIZE - 1) {
+            cout << "Stack Overflow!" << endl;
+        } else {
+            arr[++top] = value;
+            cout << value << " pushed to stack." << endl;
+        }
+    }
+
+    // Pop element from stack
+    void pop() {
+        if (top < 0) {
+            cout << "Stack Underflow!" << endl;
+        } else {
+            cout << arr[top--] << " popped from stack." << endl;
+        }
+}
