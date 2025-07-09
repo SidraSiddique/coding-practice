a49ed50a8dd43beeed1f9fbb51405b32a74a1309
Sidra Siddique <sidrasiddique494@gmail.com>
2025-07-09 20:00:37 +0500

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
-
 #define SIZE 100  
-
 class Stack {
 private:
     int arr[SIZE]; 
@@ -21,12 +19,44 @@ void push(int value) {
             cout << value << " pushed to stack." << endl;
         }
     }
-
-    // Pop element from stack
     void pop() {
         if (top < 0) {
             cout << "Stack Underflow!" << endl;
         } else {
             cout << arr[top--] << " popped from stack." << endl;
         }
+}
+int peek() {
+        if (top < 0) {
+            cout << "Stack is empty!" << endl;
+            return -1;
+        } else {
+            return arr[top];
+        }
+    }
+    bool isEmpty() {
+        return top < 0;
+    }
+    void display() {
+        if (top < 0) {
+            cout << "Stack is empty!" << endl;
+        } else {
+            cout << "Stack elements: ";
+            for (int i = top; i >= 0; i--) {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+int main() {
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.display();
+    cout << "Top element is: " << s.peek() << endl;
+    s.pop();
+    s.display();
+    return 0;
 }
